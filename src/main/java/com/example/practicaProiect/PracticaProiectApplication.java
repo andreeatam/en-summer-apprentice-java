e29f64769e04a3913a4ad6724d191a80a09a1b08
andreea.tamas31 <andreea.tamas31@gmail.com>
2023-07-19 14:27:30 +0300

    first commit

---
+++ src/main/java/com/example/practicaProiect/PracticaProiectApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicaProiect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaProiectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaProiectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/practicaProiect/PracticaProiectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicaProiect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaProiectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
